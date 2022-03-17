5c0a5c1d20e3de8e8815b4520f61505f35fbd2e0
raximvodil <raximvodilrozbek@gmail.com>
2022-03-17 17:00:52 +0500

    31:00 project started

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, AppBar, Topography, Grow, Grid } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <Container maxWidth="lg">
+
+    </Container>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://Dilrozbek_Raximov:931897318Rd@cluster0.e9gps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT} `))
+  )
+  .catch((error) => console.log(error.message));
+
+// https://www.mongodb.com/cloud/atlas

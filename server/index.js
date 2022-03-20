4fcd33e9d7ffc98d41a4a7f5298f386b9beaa4d2
raximvodil <raximvodilrozbek@gmail.com>
2022-03-20 14:07:52 +0500

    2-part project finished

--- server/index.js
+++ server/index.js
@@ -2,17 +2,21 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello to memories API");
+});
 
 const PORT = process.env.PORT || 5000;
 

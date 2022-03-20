2fdcc4d18beb2cd11ca3ad79c09e2411b566947f
raximvodil <raximvodilrozbek@gmail.com>
2022-03-20 12:58:59 +0500

    1:09

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,17 @@
-export default (posts = [], action) => {
+import { CREATE, DELETE, FETCH_ALL, LIKE, UPDATE } from "../constants/actionTypes";
+
+export default  (posts = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
+    case LIKE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
     default:
       return posts;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from 'dotenv'
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,12 +14,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://Dilrozbek_Raximov:931897318Rd@cluster0.e9gps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
